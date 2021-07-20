76557d1c9460f3dbdf06485dc316a3eb4d641ca0
zeeshan adeen <zeeshan99adeen@gmail.com>
2021-07-20 19:04:54 +0530

    fiz heroku sequelize error

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,19 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+let sequelize = '';
+
+if (process.env.NODE_ENV == 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
+	});
+} else {
+	sequelize = new Sequelize(process.env.DATABASE_URL);
+}
 
 // const init = async () => {
 // 	try {
